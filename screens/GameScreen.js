8eb11ae498d107dea306b15f9f6c103822e011fe
Egil Skjelbred <egil@skjelbred-design.no>
2022-03-22 21:43:37 +0100

    add players to GameScreen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,41 +1,45 @@
-import React from 'react'
-import { SafeAreaView, StyleSheet, Text, View, Button, route } from 'react-native'
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, View, Button, route } from "react-native";
+import { _inputs } from "./PlayerScreen";
 
-const GameScreen = ({ route }) => {
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <Text style={styles.heading}>
-          React Native Pass Value From One Screen to Another Using React
-          Navigation
-        </Text>
-        <Text style={styles.textStyle}>
-          Values passed from First page: {route.params.inputs}
-        </Text>
-      </View>
-      <Text style={{ textAlign: 'center', color: 'grey' }}>
-        www.aboutreact.com
-      </Text>
-    </SafeAreaView>
-  );
+const GameScreen = (props) => {
+    const playerArray = props.route.params.paramKey;
+    console.log(playerArray);
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={styles.container}>
+                <Text style={styles.heading}>React Native Pass Value From One Screen to Another Using React Navigation</Text>
+                <Text style={styles.textStyle}>
+                    Values passed from First page:{" "}
+                    {playerArray.map((player) => (
+                        <Text style={styles.textInputStyle}>{player.value}</Text>
+                    ))}
+                </Text>
+            </View>
+            <Text style={{ textAlign: "center", color: "grey" }}>www.aboutreact.com</Text>
+        </SafeAreaView>
+    );
 };
 
 export default GameScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    padding: 20,
-  },
-  heading: {
-    fontSize: 25,
-    textAlign: 'center',
-    marginVertical: 10,
-  },
-  textStyle: {
-    textAlign: 'center',
-    fontSize: 16,
-    marginVertical: 10,
-  },
+    container: {
+        flex: 1,
+        alignItems: "center",
+        padding: 20,
+    },
+    heading: {
+        fontSize: 25,
+        textAlign: "center",
+        marginVertical: 10,
+    },
+    textStyle: {
+        textAlign: "center",
+        fontSize: 16,
+        marginVertical: 10,
+    },
+    textInputStyle: {
+        width: "100%",
+    },
 });
--- screens/PlayerScreen.js
+++ screens/PlayerScreen.js
@@ -1,34 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import { View, Text, Button, TextInput, TouchableOpacity, StyleSheet, ScrollView } from 'react-native'
-import { useNavigation } from '@react-navigation/core';
+import React, { useEffect, useState } from "react";
+import { View, Text, Button, TextInput, TouchableOpacity, StyleSheet, ScrollView } from "react-native";
+import { useNavigation } from "@react-navigation/core";
 
 const PlayerScreen = () => {
-    const navigation = useNavigation()
-    const [inputs, setInputs] = useState([{ key: '', value: '' }]);
+    const navigation = useNavigation();
+    const [inputs, setInputs] = useState([{ key: "", value: "" }]);
 
     const addHandler = () => {
         const _inputs = [...inputs];
-        _inputs.push({ key: '', value: '' });
+        _inputs.push({ key: "", value: "" });
         setInputs(_inputs);
-    }
+    };
 
     const deleteHandler = (key) => {
         const _inputs = inputs.filter((input, index) => index != key);
         setInputs(_inputs);
-    }
+    };
 
     const inputHandler = (text, key) => {
         const _inputs = [...inputs];
         _inputs[key].value = text;
         _inputs[key].key = key;
         setInputs(_inputs);
-        console.log(_inputs)
-
-
-    }
+        console.log(_inputs);
+    };
     const goToGameScreen = () => {
-        navigation.replace("Game")
-    }
+        navigation.replace("Game");
+    };
 
     return (
         <View style={styles.container}>
@@ -39,21 +37,16 @@ const PlayerScreen = () => {
                         <TouchableOpacity style={styles.delBtn} onPress={() => deleteHandler(key)}>
                             <Text style={{ color: "red", fontSize: 20 }}>X</Text>
                         </TouchableOpacity>
-
                     </View>
                 ))}
             </ScrollView>
-            <TouchableOpacity
-                onPress={addHandler}
-                style={styles.addBtn}
-            >
+            <TouchableOpacity onPress={addHandler} style={styles.addBtn}>
                 <Text style={{ color: "white", fontSize: 20 }}>+</Text>
             </TouchableOpacity>
 
-
             <TouchableOpacity
                 onPress={() =>
-                    navigation.navigate('Game', {
+                    navigation.navigate("Game", {
                         paramKey: inputs,
                     })
                 }
@@ -63,62 +56,61 @@ const PlayerScreen = () => {
             </TouchableOpacity>
         </View>
     );
-}
+};
 
 const styles = StyleSheet.create({
     container: {
-        justifyContent: 'center',
-        alignItems: 'center',
+        justifyContent: "center",
+        alignItems: "center",
         marginTop: 60,
         marginBottom: 85,
     },
     inputContainer: {
-        flexDirection: 'row',
+        flexDirection: "row",
         //display: 'flex',
-        justifyContent: 'space-between',
-        borderColor: '#777',
+        justifyContent: "space-between",
+        borderColor: "#777",
         padding: 10,
         margin: 10,
 
         borderRadius: 10,
         //width: '60%',
-        backgroundColor: 'white',
+        backgroundColor: "white",
         paddingHorizontal: 15,
         paddingVertical: 10,
     },
 
     buttonText: {
         fontSize: 50,
-        color: 'white',
-        fontWeight: '700',
+        color: "white",
+        fontWeight: "700",
         fontSize: 16,
     },
     addBtn: {
-        backgroundColor: '#0782F9',
-        width: '13%',
+        backgroundColor: "#0782F9",
+        width: "13%",
         padding: 15,
         borderRadius: 50,
-        alignItems: 'center',
+        alignItems: "center",
         marginTop: 10,
     },
     scrollStyle: {
-        width: '100%',
+        width: "100%",
         borderWidth: 0,
     },
     delBtn: {
-        color: 'green'
+        color: "green",
     },
     goToGame: {
-        backgroundColor: '#0782F9',
-        width: '95%',
+        backgroundColor: "#0782F9",
+        width: "95%",
         padding: 15,
         borderRadius: 30,
-        alignItems: 'center',
+        alignItems: "center",
         marginTop: 10,
     },
     textInputStyle: {
         width: "100%",
-    }
-})
+    },
+});
 export default PlayerScreen;
-
